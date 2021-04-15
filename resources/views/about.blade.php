a063fb345865fc8ca794c0cc5d917fedec5cd784
Yoann <yos59540@gmail.com>
2021-04-15 09:16:17 +0200

    init

---
+++ resources/views/about.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Laravel</title>
+</head>
+<body>
+    <h1>Hello Laravel</h1>
+
+    <?php echo date('d/m/Y');
+    {{date('d/m/Y')}}
+
+    @if (1 === 1)
+        bla bla 
+    @endif
+
+    
+</body>
+</html>
